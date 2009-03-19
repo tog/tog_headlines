881d42d0d2f9cbcd0428ee100affa6c7c96ad59e
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-03-19 12:36:20 +0100

    named route for tags and new partial for tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 namespace(:headlines) do |headlines| 
   headlines.resources :stories, :collection => { :by_tag => :get} 
-  headlines.connect "/stories/tags/:tag", :controller => "stories", :action => "by_tag"
+  headlines.tag_headlines_stories "/stories/tags/:tag", :controller => "stories", :action => "by_tag"
 end
 
 namespace(:member) do |member| 
