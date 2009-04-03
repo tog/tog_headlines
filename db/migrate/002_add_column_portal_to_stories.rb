28447e753959682c71315bba23ffcc3a40bfa05a
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-04-03 14:34:42 +0200

    set new portal field to true in all existing stories

--- db/migrate/002_add_column_portal_to_stories.rb
+++ db/migrate/002_add_column_portal_to_stories.rb
@@ -2,6 +2,11 @@ class AddColumnPortalToStories  < ActiveRecord::Migration
 
   def self.up
     add_column :stories, :portal, :boolean, :default => true
+    
+    Story.find(:all).each do |story|
+      story.portal = true
+      story.save!
+    end
   end
 
   def self.down
