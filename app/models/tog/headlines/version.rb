d77ec86ef3e69f0c1380e2e4496ca2ee3c3973bb
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-06-12 10:29:47 +0200

    Release v0.4.4

---
+++ app/models/tog/headlines/version.rb
@@ -0,0 +1,17 @@
+module Version
+  MAJOR = 0
+  MINOR = 4
+  TINY  = 4
+  MODULE = "tog_headlines"
+  STRING = [MAJOR, MINOR, TINY].join('.')
+
+  class << self
+    def to_s
+      STRING
+    end
+    def full_version
+      "#{MODULE} #{STRING}"
+    end
+    alias :to_str :to_s
+  end
+end
