1aeb74d4d663b14ab5a4ee51ff0f74dc252c644f
molpe <alberto.molpeceres@gmail.com>
2009-10-13 22:13:28 +0200

    use update_tog_migration migration instead of hard-coded migration

--- tog_headlines_template.rb
+++ tog_headlines_template.rb
@@ -5,16 +5,6 @@ plugin 'tog_headlines', :git => "git://github.com/tog/tog_headlines.git"
 
 route "map.routes_from_plugin 'tog_headlines'"
 
-file "db/migrate/" + Time.now.strftime("%Y%m%d%H%M%S") + "_install_tog_headlines.rb",
-%q{class InstallTogHeadlines < ActiveRecord::Migration
-    def self.up
-      migrate_plugin "tog_headlines", 3
-    end
-
-    def self.down
-      migrate_plugin "tog_headlines", 0 
-    end
-end
-}
+generate "update_tog_migration"
 
 rake "db:migrate"
